0cb27760e6c62ccfab56113c3798be67ac871336
Kumar-Achuth <achuthkumar146@gmail.com>
2018-09-11 16:45:29 +0530

    commit

---
+++ regularExpression.js
@@ -0,0 +1,12 @@
+var prompt = require('prompt-sync')()
+var utility = require('/home/bridgeit/Documents/kumar/oops/utility/utility.js')
+function regularExpression()
+{
+   var string = prompt('Enter the string : ')
+   console.log(string)
+   var firstname = prompt('Enter the first name : ')
+   var fullname = prompt('Enter the full name :')
+   var mobile = prompt ('Enter the mobile number : ')
+   
+}
+regularExpression()
