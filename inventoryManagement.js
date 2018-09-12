69efe4020124a74cabac4b0da75fef76fbfdd1dc
Kumar-Achuth <achuthkumar146@gmail.com>
2018-09-12 18:44:35 +0530

    commit

---
+++ inventoryManagement.js
@@ -0,0 +1,47 @@
+var fs = require('fs')
+var prompt = require('prompt-sync')()
+var sum = 0
+var number_of_stocks = prompt('Enter the number of stocks : ')
+fs.readFile('./inventory.json', 'utf-8', function(err, inventory) {
+	if (err) throw err
+
+    var stock_object = JSON.parse(inventory)
+    for (var i = 0; i<=number_of_stocks-1; i++)
+{
+var name = prompt("Enter the stock name : ")
+var share = prompt('Enter the share value : ')
+var price = prompt('Enter the stock price : ')
+
+	stock_object.stock.push({
+		stockName: name,
+        sharePrice: share,
+        stockPrice : price ,
+	})
+	
+	var value = parseInt(share * price )
+	console.log('The value of the total shares is : '+value)
+	sum = sum + value
+		fs.writeFile('./inventory.json', JSON.stringify(stock_object), 'utf-8', function(err) {
+        if (err) throw err
+
+	}
+)}
+console.log(stock_object)
+console.log('The total share price  of all the stocks is ' +sum)
+var option = prompt('Enter 0 if u want to delete any stocks :')
+if(option == 0)
+{
+	var choice = prompt('Enter the index in which u want to delete the stock : ')
+delete stock_object.stock[choice] 
+}
+else 
+{
+	console.log('Thank You')
+}
+console.log(stock_object)
+fs.writeFile('./inventory.json', JSON.stringify(stock_object), 'utf-8', function(err) {
+	if (err) throw err
+}
+)
+})
+
--- regularExpression.js
+++ regularExpression.js
@@ -1,12 +1,54 @@
 var prompt = require('prompt-sync')()
-var utility = require('/home/bridgeit/Documents/kumar/oops/utility/utility.js')
-function regularExpression()
+var fs = require('fs')
+var data = fs.readFileSync('details.txt')
+data = data.toString()
+var restriction = /[a-zA-Z]/g
+var restriction1 = /[0-9]/g
+var restriction2 = /[!@#$%^&*+_]/g
+
+var name = prompt('Enter your First name : ')
+if(name.search(restriction1)==-1 && name.search(restriction2)==-1)
 {
-   var string = prompt('Enter the string : ')
-   console.log(string)
-   var firstname = prompt('Enter the first name : ')
-   var fullname = prompt('Enter the full name :')
-   var mobile = prompt ('Enter the mobile number : ')
-   
+    var string = data.replace(/<<name>>/g,name)   
 }
-regularExpression()
+else 
+    {
+        console.log('Invalid Input ')
+        process.exit()
+    }
+var fullname = prompt('Enter your Full name : ')
+
+if(fullname.search(restriction1)==-1 && fullname.search(restriction2)==-1)
+{
+    var string = string.replace(/<<full name>>/g,fullname)
+    
+}
+else 
+{
+    console.log('Invalid Input')
+    process.exit()
+}
+var contact = prompt('Enter your contact number : ')
+
+if(contact.search(restriction)==-1 && contact.search(restriction2)==-1)
+{
+    if(contact.length==10)
+    {
+        string = string.replace(/91-xxxxxxxxxx/g,contact)
+    }
+    else 
+    {
+        console.log('Enter 10 digit number')
+        process.exit()
+    }
+
+}
+else 
+{
+    console.log('invalid')
+    process.exit()
+}
+var date =  new Date()
+var d = date.getDate()+ '/'  +(date.getUTCMonth()+1)+ '/' +date.getFullYear()
+string = string.replace(/01-01-2016/g,d)
+console.log(string)
